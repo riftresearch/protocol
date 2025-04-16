7243a4917dfb80ea67e41ffecd3bfce808887e27
Cliff Syner <c4syner@gmail.com>
2025-04-16 13:39:09 -0700

    refactor(contracts): remove dependencies

--- contracts/tests/utils/MockToken.sol
+++ contracts/tests/utils/MockToken.sol
@@ -1,23 +1,45 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {ERC20} from "solady/src/tokens/ERC20.sol";
 
 // Mock Token contract (with interop with the canonical cbBTC contract)
 contract MockToken is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol, decimals) {}
+    string public $name;
+    string public $symbol;
+    uint8 public $decimals;
+
+    function name() public view virtual override returns (string memory) {
+        return $name;
+    }
+
+    /// @dev Returns the symbol of the token.
+    function symbol() public view virtual override returns (string memory) {
+        return $symbol;
+    }
+
+    /// @dev Returns the decimals places of the token.
+    function decimals() public view virtual override returns (uint8) {
+        return $decimals;
+    }
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+        $name = _name;
+        $symbol = _symbol;
+        $decimals = _decimals;
+    }
 
     function mint(address _to, uint256 _amount) public {
         _mint(_to, _amount);
     }
 
-    // Interop
+    // Interop with cbBTC
     function masterMinter() external view returns (address) {
         return msg.sender;
     }
 
-    // Interop
-    function configureMinter(address minter, uint256 minterAllowedAmount) external returns (bool) {
+    // Interop with cbBTC
+    function configureMinter(address /*minter*/, uint256 /*minterAllowedAmount*/) external pure returns (bool) {
         return true;
     }
 }
